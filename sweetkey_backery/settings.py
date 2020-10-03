04c9881196faed343b0295011cad792cb380af3f
Плосковитов Илья Антонович <iploskovitov@phoenixit.ru>
2020-10-03 23:52:48 +0300

    some fix

--- sweetkey_backery/settings.py
+++ sweetkey_backery/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # test
-STATIC_ROOT = 'static/'
+# STATIC_ROOT = 'static/'
 
 # dev
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
